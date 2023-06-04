06b72949ab207422ed511535f14428ac1e923932
bazinga <frank5d2008@gmail.com>
2023-06-04 12:45:03 +0800

    add validator & add transfer API

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/frank5487/tx_system/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 // Server serves HTTP requests for our banking service
@@ -15,6 +17,10 @@ type Server struct {
 func NewServer(store db.Store) (*Server, error) {
 	server := &Server{store: store}
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	server.setupRouter()
 
 	return server, nil
@@ -26,6 +32,8 @@ func (server *Server) setupRouter() {
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 }
