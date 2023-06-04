c512b5b345de39823991fa8a4838985f2b514018
bazinga <frank5d2008@gmail.com>
2023-06-04 10:26:07 +0800

    load const variable from configuration file with viper package

--- main.go
+++ main.go
@@ -4,24 +4,18 @@ import (
 	"database/sql"
 	"github.com/frank5487/tx_system/api"
 	db "github.com/frank5487/tx_system/db/sqlc"
+	"github.com/frank5487/tx_system/util"
 	_ "github.com/lib/pq"
 	"log"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:123456@localhost:5432/tx_system?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	//config, err := util.LoadConfig(".")
-	//if err != nil {
-	//	log.Fatal("cannot load config: ", err)
-	//}
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
 
-	//conn, err := sql.Open(config.DBDriver, config.DBSource)
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -29,8 +23,7 @@ func main() {
 	store := db.NewStore(conn)
 	server, err := api.NewServer(store)
 
-	//err = server.Start(config.ServerAddress)
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
